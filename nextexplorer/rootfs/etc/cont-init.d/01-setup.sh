39860d72a26f15b15f60fd9320578fe81fb2be27
aranaformae <fabienschmitz@hotmail.com>
2026-09-08 11:47:57 +0200

    Replace bind mounts with unprivileged symlinks

--- nextexplorer/rootfs/etc/cont-init.d/01-setup.sh
+++ nextexplorer/rootfs/etc/cont-init.d/01-setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/with-contenv bash
+set -e
+
 V_ROOT="${VOLUME_ROOT:-/storage}"
 OPTIONS_FILE="/data/options.json"
+
+mkdir -p "$V_ROOT"
 
 if jq -e '.root_volumes and (.root_volumes | length > 0)' "$OPTIONS_FILE" > /dev/null; then
     for vol in $(jq -r '.root_volumes[]' "$OPTIONS_FILE"); do
@@ -13,13 +17,29 @@ if jq -e '.root_volumes and (.root_volumes | length > 0)' "$OPTIONS_FILE" > /dev
                 SOURCE_PATH="/share"
                 TARGET_NAME="shared"
                 ;;
-            root)
-                SOURCE_PATH="/"
-                TARGET_NAME="root"
+            media)
+                SOURCE_PATH="/media"
+                TARGET_NAME="media"
+                ;;
+            backup)
+                SOURCE_PATH="/backup"
+                TARGET_NAME="backup"
+                ;;
+            all_addon_configs)
+                SOURCE_PATH="/addon_configs"
+                TARGET_NAME="addon_configs"
+                ;;
+            addons)
+                SOURCE_PATH="/addons"
+                TARGET_NAME="addons"
+                ;;
+            ssl)
+                SOURCE_PATH="/ssl"
+                TARGET_NAME="ssl"
                 ;;
             *)
-                SOURCE_PATH="/$vol"
-                TARGET_NAME="$vol"
+                echo "WARNING: unsupported root volume requested: $vol"
+                continue
                 ;;
         esac
 
@@ -27,10 +47,8 @@ if jq -e '.root_volumes and (.root_volumes | length > 0)' "$OPTIONS_FILE" > /dev
         echo "$SOURCE_PATH -> $TARGET_PATH"
 
         if [ -d "$SOURCE_PATH" ]; then
-            mkdir -p "$TARGET_PATH"
-            if ! mountpoint -q "$TARGET_PATH"; then
-                mount --bind "$SOURCE_PATH" "$TARGET_PATH"
-            fi
+            rm -rf "$TARGET_PATH"
+            ln -s "$SOURCE_PATH" "$TARGET_PATH"
         else
             echo "WARNING: source volume does not exist: $SOURCE_PATH"
         fi
@@ -39,5 +57,8 @@ else
     echo "No root_volumes defined for setup"
 fi
 
-mkdir -p "${CONFIG_DIR:-/config}/extensions/icons" "${CONFIG_DIR:-/config}/extensions/brand" "${CACHE_DIR:-/cache}/thumbnails"
+mkdir -p "${CONFIG_DIR:-/config}/extensions/icons" \
+         "${CONFIG_DIR:-/config}/extensions/brand" \
+         "${CACHE_DIR:-/cache}/thumbnails"
+
 echo "Next Explorer Setup complete."
